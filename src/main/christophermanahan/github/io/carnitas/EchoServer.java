0d043347b0443ffa978155b0c619bceb84e71bf3
Christopher Manahan <chris.a.manahan@gmail.com>
2018-12-05 21:37:07 -0500

    Refactor EchoServer

--- src/main/christophermanahan/github/io/carnitas/EchoServer.java
+++ src/main/christophermanahan/github/io/carnitas/EchoServer.java
@@ -1,8 +1,6 @@
 package christophermanahan.github.io.carnitas;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -17,16 +15,14 @@ public class EchoServer implements Server {
   public void run() {
     try {
       Socket socket = serverSocket.accept();
-      while(!socket.isClosed()) {
-        String incomingData = new BufferedReader(
-          new InputStreamReader(socket.getInputStream())
-        ).readLine();
 
-        if (incomingData == null) {
-          socket.close();
-        } else {
-          socket.getOutputStream().write(incomingData.concat("\n").getBytes());
-        }
+      Connection socketConnection = new SocketConnection(socket);
+      Input socketInput = new SocketInput(socket);
+      Output socketOutput = new SocketOutput(socket);
+
+      while(socketConnection.isOpen()) {
+        socketInput.receive()
+          .ifPresentOrElse(socketOutput::send, socketConnection::close);
       }
     } catch (IOException e) {
       throw new IllegalStateException("Connection was not accepted", e);
