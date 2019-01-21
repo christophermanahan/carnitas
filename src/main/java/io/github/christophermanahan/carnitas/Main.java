3956f1497912107d45ba9c71c850782fd08265c5
christophermanahan <chris.a.manahan@gmail.com>
2019-01-21 18:08:46 -0500

    Refactor Main to use Optional to set port

--- src/main/java/io/github/christophermanahan/carnitas/Main.java
+++ src/main/java/io/github/christophermanahan/carnitas/Main.java
@@ -2,23 +2,36 @@ package io.github.christophermanahan.carnitas;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.Optional;
 
 public class Main {
     private static ServerSocket socket;
 
     public static void main(String[] args) {
-        int port = args.length == 0 ? 33333 : Integer.parseInt(args[0]);
         Logger logger = new ErrorLogger();
-        Parser parser = new RequestParser();
-        Handler handler = new RequestHandler();
         try (
-          ServerSocket serverSocket = new ServerSocket(port);
+          ServerSocket serverSocket = new ServerSocket(port(args));
         ) {
             socket = serverSocket;
-            new HTTPServer(parser, handler, logger).start(new ConnectionListener(socket), new WhileOpen(socket));
+            new HTTPServer(
+                    new RequestParser(),
+                    new RequestHandler(),
+                    logger
+            ).start(
+                    new ConnectionListener(socket),
+                    new WhileOpen(socket)
+            );
         } catch (IOException e) {
             logger.log(e.getMessage());
         }
+    }
+
+    private static int port(String[] args) {
+        return Optional.of(args)
+                .filter(a -> a.length > 0)
+                .map(b -> b[0])
+                .map(Integer::parseInt)
+                .orElse(33333);
     }
 
     public static void stop() {
