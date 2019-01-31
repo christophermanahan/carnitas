0146c106a7f1ec9a68e079ed668dd5d106095e0f
Christopher Manahan <chris.a.manahan@gmail.com>
2019-01-31 16:26:03 -0500

    Add HeadersParser

--- src/main/java/io/github/christophermanahan/carnitas/HeadersParser.java
+++ src/main/java/io/github/christophermanahan/carnitas/HeadersParser.java
@@ -2,10 +2,13 @@ package io.github.christophermanahan.carnitas;
 
 import java.util.Iterator;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 public class HeadersParser implements Parser<Iterator<String>, Iterator<String>> {
-    public HeadersParser(RequestBuilder builder) {
+    private final RequestBuilder builder;
 
+    public HeadersParser(RequestBuilder builder) {
+        this.builder = builder;
     }
 
     public Optional<HTTPRequest> parse(Reader reader) {
@@ -13,6 +16,27 @@ public class HeadersParser implements Parser<Iterator<String>, Iterator<String>>
     }
 
     public Iterator<String> parse(Iterator<String> request) {
-        return null;
+        Headers headers = new Headers();
+        addHeaders(request, headers);
+        builder.set(headers);
+        System.out.println(builder.get().headers().get().size());
+        return request;
+    }
+
+    private void addHeaders(Iterator<String> request, Headers headers) {
+        if (addHeader(request, headers).isPresent()) {
+            addHeaders(request, headers);
+        }
+    }
+
+    private Optional<Headers> addHeader(Iterator<String> request, Headers headers) {
+        return Optional.of(request)
+          .map(Iterator::next)
+          .filter(isPresent())
+          .map(headers::add);
+    }
+
+    private Predicate<String> isPresent() {
+        return header -> !header.isEmpty();
     }
 }
--- src/test/java/io/github/christophermanahan/carnitas/HeadersParserTest.java
+++ src/test/java/io/github/christophermanahan/carnitas/HeadersParserTest.java
@@ -15,8 +15,10 @@ class HeadersParserTest {
     void itCanParseRequestHeaders() {
         HTTPRequest.Method method = HTTPRequest.Method.GET;
         int contentLength = 0;
+        String test = "Test-Header: Test";
         String restOfRequest = Headers.CONTENT_LENGTH + contentLength + Serializer.CRLF
-          + Headers.ALLOW + HTTPRequest.Method.GET
+          + Headers.ALLOW + HTTPRequest.Method.GET + Serializer.CRLF
+          + test
           + Serializer.BLANK_LINE;
         RequestBuilder builder = new RequestBuilder();
         Parser<Iterator<String>, Iterator<String>> parser = new HeadersParser(builder);
@@ -25,7 +27,27 @@ class HeadersParserTest {
 
         Headers expectedHeaders = new Headers()
           .contentLength(contentLength)
-          .allow(List.of(method));
+          .allow(List.of(method))
+          .add(test);
         assertEquals(expectedHeaders, builder.get().headers());
+    }
+
+    @Test
+    void itProvidesTheRestOfTheRequest() {
+        HTTPRequest.Method method = HTTPRequest.Method.GET;
+        int contentLength = 0;
+        String test = "Test-Header: Test";
+        String body = "name=<something>";
+        String restOfRequest = Headers.CONTENT_LENGTH + contentLength + Serializer.CRLF
+          + Headers.ALLOW + HTTPRequest.Method.GET + Serializer.CRLF
+          + test
+          + Serializer.BLANK_LINE
+          + body;
+        RequestBuilder builder = new RequestBuilder();
+        Parser<Iterator<String>, Iterator<String>> parser = new HeadersParser(builder);
+        Iterator<String> requestIterator = Arrays.asList(restOfRequest.split(Serializer.CRLF, includeEmptyStrings)).iterator();
+        parser.parse(requestIterator);
+
+        assertEquals(body, requestIterator.next());
     }
 }
